f3e956e6854b94c7a573a015b1aa512138737a91
Thomas Vincent <thomasvincent@gmail.com>
2016-03-11 09:19:58 -0800

    new client class

--- lib/webhdfs/kerberos.rb
+++ lib/webhdfs/kerberos.rb
@@ -23,7 +23,7 @@ module WebHDFS
     end
 
     # Set the token to header authorization
-    def autorization(header)
+    def authorization(header)
       encoded_token = Base64.strict_encode64(@token)
       if header
         header['Authorization'] = "Negotiate #{encoded_token}"
